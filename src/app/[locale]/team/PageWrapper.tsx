a7a0320e8ee915adf85e5e997c69ee5b03a804c9
Ting <baiyuting652@gmail.com>
2025-03-29 19:32:25 +1100

    fix prerending error

--- src/app/[locale]/team/PageWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from "next/dynamic";
-
-// Dynamically import your original Page component and disable SSR
-const Page = dynamic(() => import("./page"), { ssr: false });
-
-export default Page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import type { Metadata } from "next";
+import "./styles/globals.css";
 export const metadata: Metadata = {
-  title: 'Qrent',
-  description: 'Your Smart Rental Assistent',
+  title: "Qrent",
+  description: "Your Smart Rental Assistent",
 };
 
 export default function RootLayout({
